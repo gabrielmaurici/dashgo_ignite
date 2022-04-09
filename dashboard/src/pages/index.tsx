317b1570407f3cdfec7ee56954529518af883535
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-04-09 02:06:13 -0300

    add projeto ignite chakra

---
+++ dashboard/src/pages/index.tsx
@@ -0,0 +1,46 @@
+import { Flex, Button, Stack } from '@chakra-ui/react'
+import { Input } from '../components/Form/Input'
+
+export default function Home() {
+  return (
+    <Flex 
+      w="100vw"
+      h="100vh"
+      align="center"
+      justify="center"
+    >
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+      >
+        <Stack spacing="4">
+          <Input 
+            type="email"
+            name="email"
+            label="E-mail"
+          />
+          
+          <Input 
+            type="senha"
+            name="senha"
+            label="Password"
+          />
+        </Stack>
+
+        <Button 
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  )
+}
