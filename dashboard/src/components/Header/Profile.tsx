b636e5b1265c910c35bf02523be20626f8a85fd8
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-04-14 22:59:25 -0300

    responsividade

--- dashboard/src/components/Header/Profile.tsx
+++ dashboard/src/components/Header/Profile.tsx
@@ -1,14 +1,23 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+    showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
     return (
         <Flex align="center">
-            <Box mr="4" textAlign="right">
-                <Text>Gabriel Maurici</Text>
-                <Text color="gray.300" fontSize="small">
-                    gabriel.maurici@hotmail.com
-                </Text>
-            </Box>
+            {
+                showProfileData && (
+
+                    <Box mr="4" textAlign="right">
+                        <Text>Gabriel Maurici</Text>
+                        <Text color="gray.300" fontSize="small">
+                            gabriel.maurici@hotmail.com
+                        </Text>
+                    </Box>
+                )
+            }
 
             <Avatar
                 size="md"
