ea0966e12cafb6542e5b6e580174740b0803ff6c
Gabriel Maurici <gabriel.maurici@hotmail.com>
2022-05-13 18:16:29 -0300

    Add paginação funcional com cache useQuery

--- dashboard/src/pages/users/index.tsx
+++ dashboard/src/pages/users/index.tsx
@@ -1,10 +1,15 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Th, Thead, Tr, Text, Spinner } from "@chakra-ui/react";
+import { useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { SideBar } from "../../components/SideBar";
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
+    const [page, setPage] = useState(1);
+    const { data, isLoading, isFetching, error } = useUsers(page);
+
     return (
         <Box>
             <Header />
@@ -14,7 +19,12 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeight="normal">Usuários</Heading>
+                        <Heading size="lg" fontWeight="normal">
+                            Usuários
+                            {
+                                !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />
+                            }
+                        </Heading>
 
                         <Button
                             as="a"
@@ -26,91 +36,67 @@ export default function UserList() {
                             Criar novo
                         </Button>
                     </Flex>
-                    <Table colorScheme="whiteAlpha">
-                        <Thead>
-                            <Tr>
-                                <Th px="6" color="gray.300" width="8">
-                                    <Checkbox colorScheme="pink" />
-                                </Th>
-                                <Th>Usuário</Th>
-                                <Th>Data de cadastro</Th>
-                                <Th width="8"></Th>
-                            </Tr>
-                        </Thead>
-                        <Tbody>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Gabriel Maurici</Text>
-                                        <Text fontSize="small" color="gray.300">gabriel.maurici@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>09 de Abril, 2022</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Gabriel Maurici</Text>
-                                        <Text fontSize="small" color="gray.300">gabriel.maurici@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>09 de Abril, 2022</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                            <Tr>
-                                <Td px="6">
-                                    <Checkbox colorScheme="pink" />
-                                </Td>
-                                <Td>
-                                    <Box>
-                                        <Text fontWeight="bold">Gabriel Maurici</Text>
-                                        <Text fontSize="small" color="gray.300">gabriel.maurici@hotmail.com</Text>
-                                    </Box>
-                                </Td>
-                                <Td>09 de Abril, 2022</Td>
-                                <Td>
-                                    <Button
-                                        as="a"
-                                        size="sm"
-                                        fontSize="sm"
-                                        colorScheme="purple"
-                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                                    >
-                                        Editar
-                                    </Button>
-                                </Td>
-                            </Tr>
-                        </Tbody>
-                    </Table>
+                    {isLoading ? (
+                        <Flex justify="center">
+                            <Spinner />
+                        </Flex>
+                    ) : error ? (
+                        <Flex justify="center">
+                            <Text>
+                                Falha ao obter dados do usuários.
+                            </Text>
+                        </Flex>
+                    ) : (
+                        <>
+                            <Table colorScheme="whiteAlpha">
+                                <Thead>
+                                    <Tr>
+                                        <Th px="6" color="gray.300" width="8">
+                                            <Checkbox colorScheme="pink" />
+                                        </Th>
+                                        <Th>Usuário</Th>
+                                        <Th>Data de cadastro</Th>
+                                        <Th width="8"></Th>
+                                    </Tr>
+                                </Thead>
+                                <Tbody>
+                                    {data.users.map(user => {
+                                        return (
+                                            <Tr key={user.id}>
+                                                <Td px="6">
+                                                    <Checkbox colorScheme="pink" />
+                                                </Td>
+                                                <Td>
+                                                    <Box>
+                                                        <Text fontWeight="bold">{user.name}</Text>
+                                                        <Text fontSize="small" color="gray.300">{user.email}</Text>
+                                                    </Box>
+                                                </Td>
+                                                <Td>{user.createdAt}</Td>
+                                                <Td>
+                                                    <Button
+                                                        as="a"
+                                                        size="sm"
+                                                        fontSize="sm"
+                                                        colorScheme="purple"
+                                                        leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
+                                                    >
+                                                        Editar
+                                                    </Button>
+                                                </Td>
+                                            </Tr>
+                                        )
+                                    })}
+                                </Tbody>
+                            </Table>
 
-                    <Pagination />
+                            <Pagination
+                                totalCountOfRegisters={data.totalCount}
+                                currentPage={page}
+                                onPageChange={setPage}
+                            />
+                        </>
+                    )}
                 </Box>
             </Flex>
         </Box>
